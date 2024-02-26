3e2baab9b2946ef2ce51f0153ee843472ddbebeb
BatchAI <batchainet.gmail.com>
2024-02-27 00:28:08 +0800

    update nav content

---
+++ docs/nav/data.js
@@ -0,0 +1,37 @@
+export const navData = [
+  {
+    title: '大语言模型',
+    items: [
+      {
+        icon: 'https://nlp-eb.cdn.bcebos.com/static/eb/asset/robin.e9dc83e5.png',
+        title: '文心一言',
+        desc: '文心一言（英文名：ERNIE Bot）是百度全新一代知识增强大语言模型',
+        link: 'https://yiyan.baidu.com'
+      },
+      {
+        icon: 'https://acd-assets.alicdn.com/acd_work/tongyi/assets/logo.svg',
+        title: '通义千问',
+        desc: '通义千问，是阿里云推出的一个超大规模的语言模型',
+        link: 'https://tongyi.aliyun.com'
+      },
+      {
+        icon: 'https://cn.vuejs.org/logo.svg',
+        title: '盘古大模型',
+        desc: '盘古大模型，是华为旗下的盘古系列AI大模型，包括NLP大模型、CV大模型、科学计算大模型。',
+        link: 'https://pangu.huaweicloud.com'
+      },
+      {
+        icon: 'https://cdn-portal.hunyuan.tencent.com/public/static/logo/favicon.png',
+        title: '腾讯混元大模型',
+        desc: '腾讯混元，腾讯自主研发的通用大语言模型。',
+        link: 'https://hunyuan.tencent.com'
+      },
+      {
+        icon: 'https://chatglm.cn/img/icons/apple-touch-icon-152x152.png',
+        title: 'ChatGLM',
+        desc: '清华技术成果转化的公司智谱AI研发的支持中英双语的对话机器人',
+        link: 'https://chatglm.cn'
+      },
+    ]
+  },
+]
